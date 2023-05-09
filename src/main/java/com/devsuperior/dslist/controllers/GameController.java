091f983be8925786ba72bdca6ce62986a7e02fa9
Tieli Cristine <tielicristinesilva@gmail.com>
2023-05-09 18:39:51 -0300

    Game repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.controllers;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.services.GameService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        return gameService.findAll();
+    }
+}
---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public List<GameMinDTO> findAll() {
+        List<Game> games = gameRepository.findAll();
+        return games.stream().map(GameMinDTO::new).toList();
+    }
+}
